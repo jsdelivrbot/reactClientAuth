522892623bf0b4d1b8f1300988af05a93ac59560
marcedem <marcsea2000@gmail.com>
2017-08-31 02:35:17 +0200

    Fix issue with signin form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,5 +1,5 @@
 import React,  { Component } from  'react';
-import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 
 class Signin extends Component {
 
@@ -7,7 +7,7 @@ class Signin extends Component {
         console.log(email, password);
     }
 
-    render(){
+    render() {
 
         const { handleSubmit, fields: {email, password}} = this.props;
 
@@ -15,11 +15,11 @@ class Signin extends Component {
             <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <fieldset className="form-group">
                     <label>Email:</label>
-                    <input {...this.email} className="form-control" />
+                    <Field name="email" component="input" type="email" placeholder="Email" {...email} className="form-control" />
                 </fieldset>
                 <fieldset className="form-group">
                     <label>password:</label>
-                    <input {...password} className="form-control" />
+                    <Field name="password" component="input" type="password" placeholder="password" {...password} className="form-control" />
                 </fieldset>
                 <button action="submit" className="btn btn-primary">Sign in</button>
             </form>
