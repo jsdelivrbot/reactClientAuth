dfd72f9c2abc7719fbcf35f5f17a07365062baa1
marcedem <marcsea2000@gmail.com>
2017-09-09 06:41:51 +0200

    finish signin, signup, signout and update code base

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { BrowserRouter } from 'react-router-dom';
-import { AUTH_USER, AUTH_ERROR } from './types';
+import { UNAUTH_USER, AUTH_USER, AUTH_ERROR, FETCH_MESSAGE } from './types'
 
 const ROOT_URL = 'http://localhost:9000';
 
@@ -12,14 +12,13 @@ export function signinUser ({ email, password }){
         axios.post(`${ROOT_URL}/signin`, { email, password })
             .then(response => {
                 // if request is good ....
-                // - update state to the indicate user is authenticated
-                dispatch({ type: AUTH_USER });
 
                 // - save the JWT token
                 localStorage.setItem('token', response.data.token);
-                // - redirect o the route '/feature'
-                this.props.history.push('/feature');
-                console.log('redirecting to feature page');
+
+                // - update state to the indicate user is authenticated
+                dispatch({ type: AUTH_USER });
+
             
             })
             .catch(() => {
@@ -38,5 +37,38 @@ export function authError(error) {
         type: AUTH_ERROR,
         payload: error
     }
-  }
+}
+
+export function signoutUser() {
+    localStorage.removeItem('token')
+    return {
+        type: UNAUTH_USER
+    }
+}
   
+export function signupUser({email, password, passwordConfirmation}) {
+    return function (dispatch) {
+        axios.post(`${ROOT_URL}/signup`, {email, password, passwordConfirmation})
+        .then(response => {
+            dispatch({type: AUTH_USER})
+            localStorage.setItem('token', response.data.token)
+        })
+        .catch(({response}) => {
+            dispatch(authError(response.data.error))
+        })
+    }
+}
+
+export function fetchMessage() {
+    return function (dispatch) {
+        axios.get(ROOT_URL, {
+            headers: {authorization: localStorage.getItem('token')}
+        })
+        .then(response => {
+            dispatch({
+                type: FETCH_MESSAGE,
+                payload: response.data.message
+            })
+        })
+    }
+}
